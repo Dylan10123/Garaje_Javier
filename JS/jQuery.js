7553ef5eb19b61739d8f94e4b6f18bbd686a0110
Dylan10123 <dbds.101203@gmail.com>
2024-02-08 19:55:03 +0100

    Hotfix

--- JS/jQuery.js
+++ JS/jQuery.js
@@ -3,25 +3,21 @@ $(document).ready(function () {
 
     $('#btn-solicitud1').hover(function () {
         $('#img1').animate({
-            width: '+=15px',
-            height: '+=5px',
-        }, 200).css('box-shadow', '0px 1.3px 1.5px -8px rgba(0, 0, 0, 0.108), 0px 4.7px 3.7px -8px rgba(0, 0, 0, 0.138), 0px 10.9px 6.9px -8px rgba(0, 0, 0, 0.155), 0px 21px 12.3px -8px rgba(0, 0, 0, 0.167), 0px 37px 23px -8px rgba(0, 0, 0, 0.176), 0px 61px 55px -8px rgba(0, 0, 0, 0.19)')
+            backgroundSize: '105%',
+        }, 200)
     }, function () {
         $('#img1').animate({
-            width: '-=15px',
-            height: '-=5px'
+            backgroundSize: '100%',
         }, 200).css('box-shadow', 'none')
     })
 
     $('#btn-solicitud2').hover(function () {
         $('#img2').animate({
-            width: '+=15px',
-            height: '+=5px'
-        }, 200).css('box-shadow', '0px 1.3px 1.5px -8px rgba(0, 0, 0, 0.108), 0px 4.7px 3.7px -8px rgba(0, 0, 0, 0.138), 0px 10.9px 6.9px -8px rgba(0, 0, 0, 0.155), 0px 21px 12.3px -8px rgba(0, 0, 0, 0.167), 0px 37px 23px -8px rgba(0, 0, 0, 0.176), 0px 61px 55px -8px rgba(0, 0, 0, 0.19)')
+            backgroundSize: '105%',
+        }, 200)
     }, function () {
         $('#img2').animate({
-            width: '-=15px',
-            height: '-=5px'
+            backgroundSize: '100%',
         }, 200).css('box-shadow', 'none')
     })
 
